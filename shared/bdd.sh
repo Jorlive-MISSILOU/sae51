a464873f62da824f927e35410879325b81e124b9
Jorlive-MISSILOU <jorlivemissilou00@gmail.com>
2023-11-08 12:49:48 +0100

    conversion svg et png avec succes

--- shared/bdd.sh
+++ shared/bdd.sh
@@ -4,10 +4,13 @@
 echo 'CREATE DATABASE sae51db; USE sae51db;' > bdd.sql
 
 # Conversion deu schema dbml en sql
-dbml2sql dbmlbdd.dbml >> bdd.sql
+dbml2sql dbmlbdd.dbml >> bddcreate.sql
 
 # correction du fichier render.js
 ./svgrenderer.sh
 
 # conversion dbml en svg
-dbml-renderer dbmlbdd.dbml > bdd.svg
+dbml-renderer -i dbmlbdd.dbml -o bddimage.svg
+
+# Conversion dbml en png
+dbml-renderer -i dbmlbdd.dbml -o bddimage.png
